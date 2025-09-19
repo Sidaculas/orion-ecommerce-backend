d46ef98d3f2d233c99e26fa6c8ade5a7cbb3155c
Sidaculas <shafaat.siddhi19@gmail.com>
2025-09-19 16:18:04 +0600

    fix workflow

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 					<li className="tracking-[-.01em]">
 						Save and see your changes instantly.
 					</li>
-					<li>Update workflow</li>
+					<li>Update workflow retest</li>
 				</ol>
 
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
